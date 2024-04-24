47167333549b9905e611a54392ebd69161c94eb8
MiguelEstebanG <mikestgu@gmail.com>
2024-04-24 12:26:27 +0200

    Update docker compose

--- docker/permisos_csv.ps1
+++
@@ -1,8 +0,0 @@
-# permisos csv en nodo 1
-docker exec -u root -it node1 chmod -R 777 /opt/nifi/nifi-current/files
-
-# permisos csv en nodo 2
-docker exec -u root -it node2 chmod -R 777 /opt/nifi/nifi-current/files
-
-# permisos csv en nodo 3
-docker exec -u root -it node3 chmod -R 777 /opt/nifi/nifi-current/files
