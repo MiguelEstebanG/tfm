be28d13474c1b1e325189a6898019497bf21019e
MiguelEstebanG <mikestgu@gmail.com>
2024-05-13 13:20:56 +0200

    Despliegue local en k8s y templates

--- k8s/scripts/configuracion_entorno_k8s.sh
+++ k8s/scripts/configuracion_entorno_k8s.sh
@@ -3,25 +3,25 @@
 cd ../../carpetas_varias/input
 
 # Copiar el archivo csv al nodo 1
-kubectl exec -it nifi01-0 -- mkdir /opt/nifi/nifi-current/csv
+kubectl exec -it nifi-0 -- mkdir /opt/nifi/nifi-current/csv
 
-kubectl cp Italian_Catalogue_earthquakes.csv nifi01-0:/opt/nifi/nifi-current/csv
+kubectl cp Italian_Catalogue_earthquakes.csv nifi-0:/opt/nifi/nifi-current/csv
 
-kubectl exec -it nifi01-0 -- chmod -R 777 /opt/nifi/nifi-current/csv
+kubectl exec -it nifi-0 -- chmod -R 777 /opt/nifi/nifi-current/csv
 
 # Copiar el archivo csv al nodo 2
-kubectl exec -it nifi02-0 -- mkdir /opt/nifi/nifi-current/csv
+kubectl exec -it nifi-1 -- mkdir /opt/nifi/nifi-current/csv
 
-kubectl cp Italian_Catalogue_earthquakes.csv nifi02-0:/opt/nifi/nifi-current/csv
+kubectl cp Italian_Catalogue_earthquakes.csv nifi-1:/opt/nifi/nifi-current/csv
 
-kubectl exec -it nifi02-0 -- chmod -R 777 /opt/nifi/nifi-current/csv
+kubectl exec -it nifi-1 -- chmod -R 777 /opt/nifi/nifi-current/csv
 
 # Copiar el archivo csv al nodo 3
-kubectl exec -it nifi03-0 -- mkdir /opt/nifi/nifi-current/csv
+kubectl exec -it nifi-2 -- mkdir /opt/nifi/nifi-current/csv
 
-kubectl cp Italian_Catalogue_earthquakes.csv nifi03-0:/opt/nifi/nifi-current/csv
+kubectl cp Italian_Catalogue_earthquakes.csv nifi-2:/opt/nifi/nifi-current/csv
 
-kubectl exec -it nifi03-0 -- chmod -R 777 /opt/nifi/nifi-current/csv
+kubectl exec -it nifi-2 -- chmod -R 777 /opt/nifi/nifi-current/csv
 
 cd ../../k8s/scripts
 
