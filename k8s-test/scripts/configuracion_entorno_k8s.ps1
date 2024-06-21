287a1ef0fac8c7414114b52879b0ba51ac6fb066
Ubuntu <azureuser@tfm-ubuntu.fxsms05jojjuflunsul4sounbd.parx.internal.cloudapp.net>
2024-06-21 10:44:01 +0000

    nuevo despliegue k8s sin HorizontalPodAutoscaler (mejora el rendimiento)

---
+++ k8s-test/scripts/configuracion_entorno_k8s.ps1
@@ -0,0 +1,33 @@
+Set-Location '..\..\carpetas_varias\input'
+
+# Copiar el archivo csv al nodo 1
+kubectl exec -it nifi-0 -- mkdir /opt/nifi/nifi-current/csv
+
+kubectl cp .\Italian_Catalogue_earthquakes.csv nifi-0:/opt/nifi/nifi-current/csv
+
+kubectl exec -it nifi-0 -- chmod -R 777 /opt/nifi/nifi-current/csv
+
+# Copiar el archivo csv al nodo 2
+kubectl exec -it nifi-1 -- mkdir /opt/nifi/nifi-current/csv
+
+kubectl cp .\Italian_Catalogue_earthquakes.csv nifi-1:/opt/nifi/nifi-current/csv
+
+kubectl exec -it nifi-1 -- chmod -R 777 /opt/nifi/nifi-current/csv
+
+# Copiar el archivo csv al nodo 3
+kubectl exec -it nifi-2 -- mkdir /opt/nifi/nifi-current/csv
+
+kubectl cp .\Italian_Catalogue_earthquakes.csv nifi-2:/opt/nifi/nifi-current/csv
+
+kubectl exec -it nifi-2 -- chmod -R 777 /opt/nifi/nifi-current/csv
+
+Set-Location '..\..\k8s\scripts'
+
+# Copiar el archivo de configuracion de mongo y ejecucion en el nodo
+kubectl exec -it mongo-0 -- mkdir /scripts
+
+kubectl cp .\init_mongo.sh mongo-0:/scripts
+
+kubectl exec -it mongo-0 -- chmod +x /scripts/init_mongo.sh
+
+kubectl exec -it mongo-0 -- /scripts/init_mongo.sh
